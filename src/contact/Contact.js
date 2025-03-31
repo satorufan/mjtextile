de1f146345bd1c7fea7613f4975ade23aec408aa
kmssss <rlaalstkd177@gmail.com>
2025-03-31 17:28:58 +0900

    mjtex v1.0

--- src/contact/Contact.js
+++ src/contact/Contact.js
@@ -1,9 +1,22 @@
 import './contact.css';
 import banner from './img/banner.jpg';
 import location from './img/location.jpg';
-import inquiry from './img/inquiry.jpg';
+import inquiry from './img/inquiry3.jpg';
+import { useState } from 'react';
 
 function Contact () {
+    const [email, setEmail] = useState('');
+    const [name, setName] = useState('');
+    const [phone, setPhone] = useState('');
+    const [company, setCompany] = useState('');
+    const [message, setMessage] = useState('');
+
+    const handleEmailChange = (e) => setEmail(e.target.value);
+    const handleNameChange = (e) => setName(e.target.value);
+    const handlePhoneChange = (e) => setPhone(e.target.value);
+    const handleCompanyChange = (e) => setCompany(e.target.value);
+    const handleMessageChange = (e) => setMessage(e.target.value);
+    
     return (
         <div className="contact">
             <div className="contact-banner"
@@ -31,7 +44,7 @@ function Contact () {
                     style={{
                         backgroundImage: `url(${location})`,
                         backgroundSize: 'cover',
-                        backgroundPosition: '0% 20%',
+                        backgroundPosition: 'center',
                         backgroundRepeat: 'no-repeat'
                     }}>
                         
@@ -43,36 +56,109 @@ function Contact () {
             style={{
                 backgroundImage: `url(${inquiry})`,
                 backgroundSize: 'cover',
+                backgroundPosition: '0% 100%',
                 backgroundRepeat: 'no-repeat'
             }}>
-                <div className='contact-inquiry-frame'>
+                <div className='contact-inquiry-left'>
+                    <div className="contact-inquiry-left-content">
+                        <div className='contact-inquiry-item'>
+                            <div className='contact-inquiry-item-title'>ADDRESS</div>
+                            <div className='contact-inquiry-item-detail'>MYUNGJIN TEXTILE 2F, 256-3, Cheongsu ro, Suseong-gu, Daegu, Republic of Korea</div>
+                        </div>
+
+                        {/* Tel Section */}
+                        <div className="contact-inquiry-item">
+                            <div className="contact-inquiry-item-title"><b>TEL</b></div>
+                            <div className="contact-inquiry-item-detail">
+                                +82-53-626-2006~2
+                            </div>
+                        </div>
+
+                            {/* Fax Section */}
+                        <div className="contact-inquiry-item">
+                            <div className="contact-inquiry-item-title"><b>FAX</b></div>
+                            <div className="contact-inquiry-item-detail">+82-53-722-2414</div>
+                        </div>
+
+                            {/* Mobile Section */}
+                        <div className="contact-inquiry-item">
+                            <div className="contact-inquiry-item-title"><b>MOBILE</b></div>
+                            <div className="contact-inquiry-item-detail">
+                                +82-10-2080-2252
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className='contact-inquiry-right'>
+                    <div className='contact-inquiry-right-title'>
+                        INQUIRY
+                    </div>
+                    <div className='contact-inquiry-right-title-line'></div>
+
+                </div>
+                {/* <div className='contact-inquiry-frame'>
                     <div className='contact-inquiry-info'>
                         <div className='contact-inquiry-email'>
                             <div className='contact-inquiry-label'>Email</div>
-                            <div className='contact-inquiry-input'></div>
+                            
+                            <input
+                                type='email'
+                                className='contact-inquiry-input'
+                                value={email}
+                                onChange={handleEmailChange}
+                                placeholder='Enter your email'
+                            />
                         </div>
 
                         <div className='contact-inquiry-name'>
                             <div className='contact-inquiry-label'>Name</div>
-                            <div className='contact-inquiry-input'></div>
+                            <input
+                                type='text'
+                                className='contact-inquiry-input'
+                                value={name}
+                                onChange={handleNameChange}
+                                placeholder='Enter your name'
+                            />
                         </div>
 
                         <div className='contact-inquiry-phone'>
                             <div className='contact-inquiry-label'>Phone Number</div>
-                            <div className='contact-inquiry-input'></div>
+                            <input
+                                type='tel'
+                                className='contact-inquiry-input'
+                                value={phone}
+                                onChange={handlePhoneChange}
+                                placeholder='Enter your phone number'
+                            />
                         </div>
 
                         <div className='contact-inquiry-company'>
                             <div className='contact-inquiry-label'>Company</div>
-                            <div className='contact-inquiry-input'></div>
+                            <input
+                                type='text'
+                                className='contact-inquiry-input'
+                                value={company}
+                                onChange={handleCompanyChange}
+                                placeholder='Enter your company name'
+                            />
                         </div>
                     </div>
                     
                     <div className='contact-inquiry-content'>
                         <div className='contact-inquiry-label'>How can we help you?</div>
-                        <div className='contact-inquiry-input'></div>
+                        <textarea
+                        className='contact-inquiry-input-content'
+                        value={message}
+                        onChange={handleMessageChange}
+                        placeholder='Enter your message'
+                        />
                     </div>
-                </div>
+
+                    <div className='contact-inquiry-submit'>
+                        SEND →
+                    </div>
+                </div> */}
             </div>
         </div>
     );
